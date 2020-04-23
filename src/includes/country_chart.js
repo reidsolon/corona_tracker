69469d2aca6e4c717613dcf81b64d948ca0ba801
Ray Anthony Solon <47214116+reidsolon@users.noreply.github.com>
2020-04-23 13:22:18 +0800

    some chnages

--- src/includes/country_chart.js
+++ src/includes/country_chart.js
@@ -1,8 +1,11 @@
-export const setChart = id => {
-    var Chart = require('chart.js');
+
+
+var Chart = require('chart.js')
+export const setChart = (id, type) => {
     var ctx = document.getElementById(id);
+    console.log(ctx)
     var myChart = new Chart(ctx, {
-        type: 'line',
+        type: type,
         data: {
             labels: ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November','December'],
             datasets: [{
@@ -10,19 +13,6 @@ export const setChart = id => {
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
             }]
@@ -37,6 +27,5 @@ export const setChart = id => {
             }
         }
     });
-
-    myChart
+    console.log(myChart)
 }
---
+++ src/includes/mapbox.js
@@ -0,0 +1,16 @@
+
+export const mapbox = (
+    accessToken,
+    mapStyle,
+) => {
+    console.log(accessToken)
+    console.log(mapStyle)
+    // mapboxgl.accessToken = accessToken;
+    // var map = new mapboxgl.Map({
+    //     container: 'map', 
+    //     style: mapStyle,
+    //     center: [0,0], 
+    //     zoom: 9
+    // });
+    // console.log(map)
+}
